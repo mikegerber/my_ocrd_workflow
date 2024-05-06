956de7492f9061e78d473260ef3f12ef50b8a577
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2024-05-06 23:40:55 +0200

    🐛 Fix/complete ocrd_cis support

--- test-ocrd_cis.sh
+++ test-ocrd_cis.sh
@@ -4,11 +4,27 @@ set -ex
 test_id=`basename $0`
 cd `mktemp -d /tmp/$test_id-XXXXX`
 
+OCRD_CIS_OCROPY_MODEL=fraktur.pyrnn.gz
+
+# Prepare processors
+ocrd resmgr download ocrd-cis-ocropy-recognize $OCRD_CIS_OCROPY_MODEL
+
 # Prepare test workspace
-wget https://qurator-data.de/examples/actevedef_718448162.first-page+binarization+segmentation.zip
-unzip actevedef_718448162.first-page+binarization+segmentation.zip
-cd actevedef_718448162.first-page+binarization+segmentation
+wget https://qurator-data.de/examples/actevedef_718448162.first-page.zip
+unzip actevedef_718448162.first-page.zip
+cd actevedef_718448162.first-page
+
+# XXX ocrd-cis-ocropy-segment wasn't happy with the binarized input (no
+# "binarized" AlternativeImage?!), so we do it here again
+ocrd-skimage-binarize -I OCR-D-IMG -O OCR-D-IMG-BIN
 
 # Run tests
-ocrd-cis-ocropy-segment -I OCR-D-IMG-BIN -O TEST-CIS-OCRPY-SEGMENT
-# TODO -recognize
+ocrd-cis-ocropy-segment \
+   -I OCR-D-IMG-BIN -O TEST-CIS-OCROPY-SEG-LINE \
+   -P level-of-operation page
+test "$(grep TextLine TEST-CIS-OCROPY-SEG-LINE/*.xml | wc -l)" -gt 50
+
+ocrd-cis-ocropy-recognize \
+   -I TEST-CIS-OCROPY-SEG-LINE -O TEST-CIS-OCROPY-OCR \
+   -P model $OCRD_CIS_OCROPY_MODEL
+test "$(grep Unicode TEST-CIS-OCROPY-OCR/*.xml | wc -l)" -gt 50
